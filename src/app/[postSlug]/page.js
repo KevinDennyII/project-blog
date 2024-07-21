c824feb43d6fda3611e8cd7aa274d21e2458d3dd
Kevin Denny II <kevin@kronickdigitalmedia.com>
2024-07-20 23:51:29 -0500

    Exercise 2: rendering an MDX component to properly handle mdx context blog post

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -3,21 +3,20 @@ import React from 'react';
 import BlogHero from '@/components/BlogHero';
 
 import styles from './postSlug.module.css';
+import { loadBlogPost } from "@/helpers/file-helpers";
+import { MDXRemote } from 'next-mdx-remote/rsc';
 
-function BlogPost() {
+async function BlogPost({params}) {
+
+  // grabbing params from dynamic routing
+  const {frontmatter, content} = await loadBlogPost(params.postSlug)
   return (
     <article className={styles.wrapper}>
       <BlogHero
-        title="Example post!"
-        publishedOn={new Date()}
+        {...frontmatter}
       />
       <div className={styles.page}>
-        <p>This is where the blog post will go!</p>
-        <p>
-          You will need to use <em>MDX</em> to render all of
-          the elements created from the blog post in this
-          spot.
-        </p>
+        <MDXRemote source={content} />
       </div>
     </article>
   );

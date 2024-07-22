808d0ea7ea7b212abc3d704f0ba513a7955078aa
Kevin Denny II <kevin@kronickdigitalmedia.com>
2024-07-22 12:47:15 -0500

    exercise 4: updated MDX remote component by passing components param and rendering the pre tag with the CodeSnippet component

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import BlogHero from '@/components/BlogHero';
+import CodeSnippet from "@/components/CodeSnippet";
 
 import styles from './postSlug.module.css';
 import { loadBlogPost } from "@/helpers/file-helpers";
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import {BLOG_TITLE} from "@/constants";
+
 
 export async function generateMetadata({ params }) {
   const blogContent = await loadBlogPost(params.postSlug)
@@ -21,13 +23,16 @@ async function BlogPost({params}) {
 
   // grabbing params from dynamic routing
   const {frontmatter, content} = await loadBlogPost(params.postSlug)
+
   return (
     <article className={styles.wrapper}>
       <BlogHero
         {...frontmatter}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote source={content} components={{
+          pre: CodeSnippet,
+        }} />
       </div>
     </article>
   );

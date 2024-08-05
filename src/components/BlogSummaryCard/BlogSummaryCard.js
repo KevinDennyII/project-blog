56c064105d650370299e433240d613abfaac03eb
Kevin Denny II <kevin@kronickdigitalmedia.com>
2024-08-05 01:09:17 -0500

    added prefetch attribute for a smoother user experience.  This is granted by invoking some preloading of the page

--- src/components/BlogSummaryCard/BlogSummaryCard.js
+++ src/components/BlogSummaryCard/BlogSummaryCard.js
@@ -20,7 +20,7 @@ function BlogSummaryCard({
 
   return (
     <Card className={styles.wrapper}>
-      <Link href={href} className={styles.title}>
+      <Link prefetch={true} href={href} className={styles.title}>
         {title}
       </Link>
       <time dateTime={publishedOn}>{humanizedDate}</time>
